d5e19513241d752a39a26472660d6f0d01bc209e
Alicja Halamska <alicja.halamska@allegro.pl>
2021-08-27 11:34:03 +0200

    zad3

--- src/Trip.js
+++ src/Trip.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
 import {Col, Container, Row, Table} from "react-bootstrap";
+import {gql, useQuery} from "@apollo/client";
 
 
 const Trip = () => {
 
+        const {loading, error, data} = useQuery(TRIP_QUERY);
+        console.log(data)
 
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
 
         return (
             <div>
                 <h2 className="Header">
-                    TripName (tripId)
+                    {data.trip.name} ({data.trip.id})
                 </h2>
                 <Container fluid>
                     <Row>
@@ -19,9 +24,9 @@ const Trip = () => {
                                 <Card.Body>
                                     <Card.Title>Info</Card.Title>
 
-                                    <Card.Text>place :</Card.Text>
-                                    <Card.Text>description :</Card.Text>
-                                    <Card.Text>pricePln :</Card.Text>
+                                    <Card.Text>place : {data.trip.place}</Card.Text>
+                                    <Card.Text>description : {data.trip.description}</Card.Text>
+                                    <Card.Text>price : {data.trip.pricePln} zł</Card.Text>
                                 </Card.Body>
                             </Card>
                         </Col>
@@ -30,10 +35,10 @@ const Trip = () => {
                                 <Card.Body>
                                     <Card.Title>Owner</Card.Title>
 
-                                    <Card.Text>name: </Card.Text>
-                                    <Card.Text>gender: </Card.Text>
-                                    <Card.Text>age: </Card.Text>
-                                    <Card.Text>email: </Card.Text>
+                                    <Card.Text>name: {data.trip.owner.firstName} {data.trip.owner.surname}</Card.Text>
+                                    <Card.Text>gender: {data.trip.owner.gender.toLowerCase()}</Card.Text>
+                                    <Card.Text>age: {data.trip.owner.age}</Card.Text>
+                                    <Card.Text>email: {data.trip.owner.email}</Card.Text>
 
                                 </Card.Body>
                             </Card>
@@ -55,27 +60,16 @@ const Trip = () => {
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Mark Otto</td>
-                                            <td>MALE</td>
-                                            <td>27</td>
-                                            <td>email@mdo</td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>Mark Otto</td>
-                                            <td>MALE</td>
-                                            <td>27</td>
-                                            <td>email@mdo</td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td>Mark Otto</td>
-                                            <td>MALE</td>
-                                            <td>27</td>
-                                            <td>email@mdo</td>
-                                        </tr>
+                                        {data.trip.participants.map(
+                                            participant =>
+                                                <tr>
+                                                    <td>{participant.id}</td>
+                                                    <td>{participant.firstName} {participant.surname}</td>
+                                                    <td>{participant.gender}</td>
+                                                    <td>{participant.age}</td>
+                                                    <td>{participant.email}</td>
+                                                </tr>)
+                                        }
                                         </tbody>
                                     </Table>
                                 </Card.Body>
@@ -87,5 +81,41 @@ const Trip = () => {
         )
     }
 ;
+
+const TRIP_QUERY = gql`
+    query Trip {
+        trip(id: "1") {
+            __typename
+            ... on Trip {
+                id
+                name
+                place
+                description
+                pricePln
+                owner {
+                    __typename
+                    ... on User {
+                        firstName
+                        surname
+                        gender
+                        age
+                        email
+                    }
+                }
+                participants(limit: 10) {
+                    id
+                    firstName
+                    surname
+                    gender
+                    age
+                    email
+                }
+            }
+            ... on TripNotFound {
+                notFoundId: id
+            }
+        }
+    }
+`
 
 export default Trip;
